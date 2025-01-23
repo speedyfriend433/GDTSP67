4cf65315221504942a629f682c2d9dcc91b8ca61
speedyfriend433 <speedyfriend13@gmail.com>
2025-01-24 01:43:42 +0900

    Create App.tsx

---
+++ public/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Results from './pages/Results';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
